1190610f88a7f63beb7ee682b40ab1b6c976a266
Ramil599 <68122372+Ramil599@users.noreply.github.com>
2022-03-25 18:22:41 +0300

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
-
-namespace ConsoleApp1
+using System.IO;
+interface ILogger
 {
+    void LogInfo(string message);
+    void LogWarning(string message);
+    void LogError(string message, Exception ex);
+
+}
+namespace ConsoleApp4
+
+{
+    class LocalFileLogger<T> : ILogger
+    {
+
+        public string GenericTypeName = typeof(T).Name;
+        private string Way;
+        public LocalFileLogger(string way)
+        {
+            Way = way;
+        }
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(Way, $"[Info]:\n [{GenericTypeName}]:{message}\n");
+        }
+        public void LogWarning(string message)
+        {
+            File.AppendAllText(Way, $"[Warning]:\n [{GenericTypeName}]:{message}\n");
+        }
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllText(Way, $"[Error]:\n [{GenericTypeName}]:{message}. {ex.Message}\n");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string way = "1.txt";
+            var LoggerInt = new LocalFileLogger<int>(way);
+            var LoggerStr = new LocalFileLogger<string>(way);
+            var LoggerChar = new LocalFileLogger<char>(way);
+            try
+            {
+                LoggerChar.LogInfo("Hello");
+                LoggerChar.LogWarning("Hello");
+                LoggerInt.LogInfo("Hi");
+                LoggerInt.LogWarning("Hi");
+                LoggerStr.LogInfo("How");
+                LoggerStr.LogWarning("How");
+                throw new IndexOutOfRangeException();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                LoggerChar.LogError("Bye", e);
+                LoggerStr.LogError("Bye", e);
+                LoggerInt.LogError("Bye", e);
+            }
         }
     }
 }
